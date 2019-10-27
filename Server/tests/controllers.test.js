5989443eb27adaac79b37db4e2e37c7b023eb240
Naoto Imamachi <naoto.imamachi@gmail.com>
2019-10-27 10:32:42 +0900

    tested POST method endpoint for channels table

--- Server/tests/controllers.test.js
+++ Server/tests/controllers.test.js
@@ -22,7 +22,7 @@ describe("channel controller", () => {
     request = chai.request(setupServer(models));
   });
 
-  describe("#list", () => {
+  describe("GET - /api/channels", () => {
     const newChannel = {
       channel_title_jp: "VTuber Naoto JP",
       channel_title_en: "VTuber Naoto EN",
@@ -42,6 +42,34 @@ describe("channel controller", () => {
         newChannel.channel_title_en
       );
       expect(res.body[0].channel_id).to.be.eq(newChannel.channel_id);
+    });
+  });
+
+  describe("POST - /api/channels/:id", () => {
+    const newChannel = {
+      channel_title_jp: "VTuber Naoto JP",
+      channel_title_en: "VTuber Naoto EN",
+      channel_id: "XXXXX",
+      thumbnail: "http://xxxx",
+    };
+
+    after(() => knex(TABLE_CHANNELS).delete());
+
+    it("should create a channel", async () => {
+      const res = await request.post("/api/channels/").send(newChannel);
+      expect(res.body.id).to.be.a("number");
+      expect(res.body.channel_title_jp).to.be.eq(newChannel.channel_title_jp);
+      expect(res.body.channel_title_en).to.be.eq(newChannel.channel_title_en);
+      expect(res.body.channel_id).to.be.eq(newChannel.channel_id);
+      expect(res.body.thumbnail).to.be.eq(newChannel.thumbnail);
+      expect(res.body.view_count).to.be.eq(0);
+      expect(res.body.subscriber_count).to.be.eq(0);
+      expect(res.body.video_count).to.be.eq(0);
+    });
+
+    it("should return err message when a duplicate channel name is provided", async () => {
+      const res = await request.post("/api/channels").send(newChannel);
+      expect(res.text).to.be.eq("That channel already exists");
     });
   });
 });
